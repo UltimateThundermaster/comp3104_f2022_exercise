74483cf82a869a8eb47d82a03603a2d68d66a593
Artem Elagin <artem.elagin@georgebrown.ca>
2022-10-08 17:25:04 -0400

    React project code added

---
+++ build/precache-manifest.76933d1df96e921d73993b26dc23952f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0b836405a19b8af612a7",
+    "url": "/UltimateThundermaster/comp3104_f2022_exercise/static/css/main.fd4d9bc9.chunk.css"
+  },
+  {
+    "revision": "0b836405a19b8af612a7",
+    "url": "/UltimateThundermaster/comp3104_f2022_exercise/static/js/main.0b836405.chunk.js"
+  },
+  {
+    "revision": "7626fc203b52b1ed4ba9",
+    "url": "/UltimateThundermaster/comp3104_f2022_exercise/static/js/runtime~main.7626fc20.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/UltimateThundermaster/comp3104_f2022_exercise/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "4f5f9ba7b839cc424713a19dc7873747",
+    "url": "/UltimateThundermaster/comp3104_f2022_exercise/index.html"
+  }
+];
